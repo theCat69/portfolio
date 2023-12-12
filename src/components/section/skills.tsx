a11b10e7072de35b154e2f9e474d6275d811f25c
Felix Vadcard <vadcard.felix@gmail.com>
2023-12-12 15:24:27 +0100

    skill cards

--- src/components/section/skills.tsx
+++ src/components/section/skills.tsx
@@ -5,19 +5,17 @@ import SkillCard from "./skills/skill-card";
 export default component$(() => {
   return (
     <>
-      <section id="my-skills" class="w-full min-h-screen bg-dark-1 padding">
-        <div class="min-h-screen flex flex-col justify-center md:gap-16 gap-8">
-          <div class="flex flex-row justify-between items-center">
+      <section id="my-skills" class="w-full min-h-screen bg-dark-1">
+        <div class="min-h-screen flex flex-col justify-evenly gap-4">
+          <div class="flex flex-row justify-center">
             <h1 class="text-4xl text-bold text-primary">My skills</h1>
           </div>
           <div class="w-full h-full flex justify-center items-center">
-            <div class="w-full h-full grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 grid-flow-row gap-4 justify-evenly items-center xl:px-32 lg:px-16 md:px-8 sm:px-4 px-0">
+            <div class="w-full h-full grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 grid-flow-row auto-rows-auto md:gap-8 gap-4 justify-evenly items-center xl:px-48 lg:px-16 md:px-8 sm:px-4 px-0">
               {skills.map((skill) => (
-                <div key={skill.id}>
-                  <SkillCard
-                    {...skill}
-                  />
-                </div>
+                <SkillCard
+                  {...skill}
+                />
               ))}
             </div>
           </div>
--- src/components/section/skills/skill-card.tsx
+++ src/components/section/skills/skill-card.tsx
@@ -4,7 +4,7 @@ import { Skill } from "~/constants";
 export default component$((props: Skill) => {
   return (
     <>
-      <div class="flex flex-col bg-light-1 relative rounded-xl">
+      <div key={props.id} class="min-h-full flex flex-col bg-primary relative rounded-xl">
         <div class="flex flex-row justify-between absolute w-full py-2 px-4">
           {props.imgs.map((img, index) => (
             <img key={index} src={img} alt="icon" width={40} height={40} />
@@ -12,9 +12,9 @@ export default component$((props: Skill) => {
         </div>
         <div class="flex flex-col gap-4 px-8 py-4">
           <div class="flex flex-row justify-center">
-            <h1 class="text-2xl text-bold">{props.title}</h1>
+            <h1 class="text-2xl text-bold text-dark-1">{props.title}</h1>
           </div>
-          <div class="flex flex-row justify-normal">
+          <div class="flex flex-row justify-normal text-xl text-dark-2 md:px-8 lg:py-8 px-2 py-2">
             <p>{props.description}</p>
           </div>
         </div>
