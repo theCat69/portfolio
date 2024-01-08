770659756e2855647641477774789c5c215fdb62
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-08 18:02:38 +0100

    adding chown to deploy

--- jenkins-scripts/deploy.sh
+++ jenkins-scripts/deploy.sh
@@ -1,3 +1,4 @@
 rm -rf "$PORTFOLIO_PATH"/*
 cp -r "$BUILD_PATH"/* "$PORTFOLIO_PATH"/
 find "$PORTFOLIO_PATH" -type f -exec chmod 664 -- {} +
+find "$PORTFOLIO_PATH" -exec chown staticweb:staticweb -- {} +
