af5dd6d4d4ae783e270f57b2a55a4cf8999e4ddf
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-08 18:26:51 +0100

    fixing chown now jenkins can change ownership to static web for that directory

--- jenkins-scripts/deploy.sh
+++ jenkins-scripts/deploy.sh
@@ -1,4 +1,4 @@
 rm -rf "$PORTFOLIO_PATH"/*
 cp -r "$BUILD_PATH"/* "$PORTFOLIO_PATH"/
 find "$PORTFOLIO_PATH" -type f -exec chmod 664 -- {} +
-find "$PORTFOLIO_PATH" -exec chown staticweb:staticweb -- {} +
+sudo chown -R staticweb:staticweb "$PORTFOLIO_PATH"
