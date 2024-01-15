060c9837aba62b61da9c140ef440934f076c464c
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-15 11:38:35 +0100

    fixing directory rights on deploy

--- jenkins-scripts/deploy.sh
+++ jenkins-scripts/deploy.sh
@@ -1,4 +1,5 @@
 rm -rf "$PORTFOLIO_PATH"/*
 cp -r "$BUILD_PATH"/* "$PORTFOLIO_PATH"/
 find "$PORTFOLIO_PATH" -type f -exec chmod 664 -- {} +
+find "$PORTFOLIO_PATH" -type d -exec chmod 775 -- {} +
 sudo chown -R staticweb:staticweb "$PORTFOLIO_PATH"
