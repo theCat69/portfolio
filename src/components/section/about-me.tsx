a8ab0d30c8562d5fdcf2de862234d9dc4bfa137e
Felix Vadcard <vadcard.felix@gmail.com>
2024-02-13 10:26:55 +0100

    rephrasing and correcting typo with IA

--- src/components/section/about-me.tsx
+++ src/components/section/about-me.tsx
@@ -10,7 +10,12 @@ export default component$(() => {
       <section id="about-me" class="w-full flex justify-center min-h-screen bg-middle">
         <div class="h-min-screen flex flex-col text-center items-center justify-center gap-8 px-16 py-8">
           <h1 class="text-4xl text-white font-salsa">{t('app.aboutme')}</h1>
-          <p class="text-white text-xl lg:text-2xl font-baskerville">{t('aboutme.text@@Born in Cannes in 1988. I grew up between the mountains and the sea. I played basketball for more than 10 years and I am still a fan of the NBA. I learned guitar in my late teenage years and now I still play for myself (and my neighboors). I went to Nice university of science to study chemistery and I pursued my master in Lyon. After struggling to find my place in the chemistery industry I pursued a formation to be a full stack developer. At the end of the formation, I was recrueted by Cap Gemini, a famous tech consulting French compagny. After 3 years working on different Java project I was recrueted by Webhelp payment services to perform java development. Today, I am pursuing my own path as an independant full stack developer and I am looking forward for us working together.')}</p>
+          <div class="flex flex-col text-center items-center justify-center gap-4">
+            <p class="text-white text-xl lg:text-2xl font-baskerville">{t('aboutme.text')}</p>
+            <p class="text-white text-xl lg:text-2xl font-baskerville">{t('aboutme.para2')}</p>
+            <p class="text-white text-xl lg:text-2xl font-baskerville">{t('aboutme.para3')}</p>
+            <p class="text-white text-xl lg:text-2xl font-baskerville">{t('aboutme.para4')}</p>
+          </div>
         </div>
       </section>
     </>
